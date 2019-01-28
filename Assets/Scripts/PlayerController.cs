af1ae7aaa432f348d2adb9432bad521200c04ebd
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-01-28 14:41:08 +0100

    oui

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,26 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigid;
     Vector3 velocity = Vector3.zero;
     public float Speed = 10;
+    Transform shotPoint;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        shotPoint = GameObject.Find("shotPoint").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
-        
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            NormalShot();
+        }
+    }
+
+    void NormalShot()
+    {
+        Instantiate(Resources.Load("Prefabs/normalShot"),shotPoint.transform.position,shotPoint.transform.rotation);
     }
 
     void Move()
