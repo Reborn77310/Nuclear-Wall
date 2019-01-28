2e91c408f5359314868b8e0af87222e61604b1ad
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-01-28 14:29:37 +0100

    débuts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Vector3 velocity = Vector3.zero;
+    public float Speed = 10;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        
+    }
+
+    void Move()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        velocity.x = horizontal * Time.deltaTime * Speed;
+        rigid.velocity = velocity;
+    }
+}
