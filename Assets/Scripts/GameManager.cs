6651ce89d9ae21caf8387cd5806609aa173b3835
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-02-01 23:30:29 +0100

    LUMIERE DE BITE

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 	public GameObject[] DifferentCubes;
-	
+	Vector3 positionOnY = new Vector3(0,0.5f,0);
+	private void Start()
+	{
+		StartCoroutine("BaisserLesCubes");
+	}
+
 	public void SpawnCube(GameObject cubeHit)
 	{
 		var random = Random.Range(0,4);
-		var positionOnY = new Vector3(0,0.5f,0);
 		var registerName = DifferentCubes[random].name;
 		GameObject wantedCube = Instantiate(DifferentCubes[random], cubeHit.transform.position - positionOnY,Quaternion.identity);
 		wantedCube.name = registerName;
+	}
+
+	IEnumerator BaisserLesCubes()
+	{
+		yield return new WaitForSeconds(5);
+		foreach (var go in GameObject.FindGameObjectsWithTag("Cubes"))
+		{
+			go.transform.position -= positionOnY;
+		}
+		StartCoroutine("BaisserLesCubes");
 	}
 }
